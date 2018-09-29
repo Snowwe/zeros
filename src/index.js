f4cd7b1319113b1c2206080d6af841ef27c02ad3
Snezhana <snowwe25@rambler.ru>
2018-09-29 04:30:45 +0300

    version_1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    if (number < 0)
+        return false;
+    else {
+        let result = 1;
+        for (let i = 10; i <= number; ++i) {
+            let temp = i;
+            while (temp % 5 === 0) {
+                temp /= 5;
+                ++result;
+            }
+        }
+        return result;
+    }
+};
